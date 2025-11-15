35d16a5985dbfcae2a71f32789818771fe565423
Yash Panchal <panchalyash089@gmail.com>
2025-11-15 13:24:36 +0530

    SMTP Mail Transfer

---
+++ src/test/java/com/yash/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,21 @@
+package com.yash.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail() {
+        emailService.sendEmail("panchalyash089@gmail.com",
+                "Testing Java Mail Sender",
+                "Hi, kaisa laga ye mera test email?");
+    }
+
+
+}
