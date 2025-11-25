f76e88de2ffd494b1c9eea0d16a5751cf5f1a047
Yash Panchal <panchalyash089@gmail.com>
2025-11-25 12:25:53 +0530

    Did ordering

--- src/main/java/com/yash/journalApp/config/SwaggerConfig.java
+++ src/main/java/com/yash/journalApp/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.yash.journalApp.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +23,14 @@ public class SwaggerConfig {
                         .version("1.0")
                 )
                 .servers(Arrays.asList(new Server().url("http://localhost:8080/").description("Local Server"),
-                        new Server().url("http://localhost:8081/").description("Live Server")));
+                        new Server().url("http://localhost:8081/").description("Live Server")))
+
+                .tags(Arrays.asList(
+                        new Tag().name("Public APIs"),
+                        new Tag().name("User APIs"),
+                        new Tag().name("Journal APIs"),
+                        new Tag().name("Admin APIs")
+
+                ));
     }
 }
