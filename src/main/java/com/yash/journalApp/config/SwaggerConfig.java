9f3b605882314049c16213230ee6239df1f4bd70
Yash Panchal <panchalyash089@gmail.com>
2025-11-25 13:53:38 +0530

    Added Security

--- src/main/java/com/yash/journalApp/config/SwaggerConfig.java
+++ src/main/java/com/yash/journalApp/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.yash.journalApp.config;
 
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +35,15 @@ public class SwaggerConfig {
                         new Tag().name("Journal APIs"),
                         new Tag().name("Admin APIs")
 
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes(
+                        "bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
                 ));
     }
 }
