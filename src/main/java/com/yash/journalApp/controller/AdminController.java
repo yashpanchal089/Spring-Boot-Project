d219ea7ad48a528a6f07361c6880688d2f476917
Yash Panchal <panchalyash089@gmail.com>
2025-11-26 14:47:20 +0530

    Added Tag

--- src/main/java/com/yash/journalApp/controller/AdminController.java
+++ src/main/java/com/yash/journalApp/controller/AdminController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
-@Tag(name = "Admin APIs")
+@Tag(name = "Admin APIs", description = "Admin level operations")
 
 public class AdminController {
 
